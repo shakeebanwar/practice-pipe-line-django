d553e57033a55975e63e630260323355567644f1
shakeeb <shakeebanwar250@gmail.com>
2021-08-14 21:55:23 +0500

    done

--- counter/settings.py
+++ counter/settings.py
@@ -78,7 +78,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             # "hosts": [('127.0.0.1', 6379)],
-            "hosts": [('18.119.140.171', 6379)],
+            "hosts": [('redis', 6379)],
         },
     },
 }
