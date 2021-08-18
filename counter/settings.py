b265e32668a1b3a2a43bfaea37f1445e8c296518
shakeeb <shakeebanwar250@gmail.com>
2021-08-18 22:42:23 +0500

    backup

--- counter/settings.py
+++ counter/settings.py
@@ -160,7 +160,7 @@ AWS_STORAGE_BUCKET_NAME = 'kroon-beta'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_REGION_NAME = 'us-east-1'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
